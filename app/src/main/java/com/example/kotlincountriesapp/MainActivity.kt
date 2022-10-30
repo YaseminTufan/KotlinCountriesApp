9faa68dfc52f9d48f0b27c96a387242aca3cb9b0
Yasemin Tufan <yasemin_ucari@hotmail.com>
2022-10-30 22:14:25 +0300

    first delete

--- app/src/main/java/com/example/kotlincountriesapp/MainActivity.kt
+++ app/src/main/java/com/example/kotlincountriesapp/MainActivity.kt
@@ -9,14 +9,5 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    override fun finish() {
-        super.finish()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-    }
-
-
 
 }
