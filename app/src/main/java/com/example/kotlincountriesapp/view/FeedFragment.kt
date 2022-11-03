f9e455a0c1ce2dc0a1199748785f1581d4b3b983
Yasemin Tufan <yasemin_ucari@hotmail.com>
2022-11-03 14:52:28 +0300

    FeedViewModel.kt created FeedFragment.kt set view model

--- app/src/main/java/com/example/kotlincountriesapp/view/FeedFragment.kt
+++ app/src/main/java/com/example/kotlincountriesapp/view/FeedFragment.kt
@@ -5,9 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.kotlincountriesapp.R
+import com.example.kotlincountriesapp.adapter.CountryAdapter
+import com.example.kotlincountriesapp.viewmodel.FeedViewModel
+import kotlinx.android.synthetic.main.fragment_feed.*
 
 class FeedFragment : Fragment() {
+
+    private lateinit var viewModel : FeedViewModel
+    private val countryAdapter = CountryAdapter(arrayListOf())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,5 +35,49 @@ class FeedFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        initViewModel()
+        setRecyclerView(view)
+        observeLiveData()
+
+
+
+    }
+    private fun initViewModel(){
+        viewModel = ViewModelProviders.of(this).get(FeedViewModel::class.java)
+        viewModel.refreshData()
+    }
+    fun setRecyclerView(view: View) {
+        countryList.layoutManager = LinearLayoutManager(context)
+        countryList.adapter = countryAdapter
+
+    }
+    fun observeLiveData() {
+        viewModel.countries.observe(viewLifecycleOwner, Observer { countries ->
+            countries?.let {
+                //countries boş değilse recycler viewi gösterip (countries) countryError ve Loadingi göstermeyiz!
+                countryList.visibility = View.VISIBLE
+                countryAdapter.updateCountryList(countries)
+            }
+        })
+        viewModel.countryError.observe(viewLifecycleOwner, Observer { error->
+            error?.let {
+                if (it) { //boolean true ise yani hata mesajı var ise
+                   countryError.visibility = View.VISIBLE
+                }else {
+                    countryError.visibility = View.GONE
+                }
+            }
+        })
+        viewModel.countryLoading.observe(viewLifecycleOwner, Observer { loading->
+            loading?.let {
+                if (it) {
+                    countryLoading.visibility = View.VISIBLE
+                    countryList.visibility = View.GONE
+                    countryError.visibility = View.GONE
+                }else {
+                    countryLoading.visibility = View.GONE
+                }
+            }
+        })
     }
 }
